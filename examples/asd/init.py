584bea0237c45634afab74e2b1917145220f92e4
mokny <33606734+mokny@users.noreply.github.com>
2023-12-01 13:46:03 +0100

    run mods

--- examples/asd/init.py
+++ examples/asd/init.py
@@ -1,23 +1,10 @@
 import time
 import os
 import sys
-import mokka
 import uuid
 
 def in_venv():
     return sys.prefix != sys.base_prefix
-
-def eventHandler(msg):
-    print("EVENT " + str(msg))
-    print("Making a STATUS request...")
-    rid = mokka.request('STATUS','')
-    print("RequestID: " + rid)
-
-def responseHandler(requestid, data):
-    print("Got a response to " + requestid + ': ' + str(data))
-
-mokka.setEventHandler(eventHandler)
-mokka.setResponseHandler(responseHandler)
 
 print("Virtual Environment: " + str(in_venv()))
 
