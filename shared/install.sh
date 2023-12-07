70b69c27cbd46b070021e68ff9bf7c64f3af30f1
mokny <33606734+mokny@users.noreply.github.com>
2023-12-07 11:29:37 +0100

    install

--- shared/install.sh
+++ shared/install.sh
@@ -7,12 +7,17 @@ DIR="~/mokka"
 DIR="/etc"
 
 if [ -d "$DIR" ]; then
-    read -n1 -p "There is already a mokka directory. Remove? [Y,n]" doit 
-    case $doit in  
-    y|Y) rm -rf ./mokka ;; 
-    n|N) exit 0 ;;
-    *) rm -rf ./mokka ;; 
-    esac
+
+    read -p "Are you sure? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm -rf ./mokka ;; 
+    else
+        exit 0
+        # do dangerous stuff
+    fi
+
 else
     echo "All ok, cloning repo"
 fi
