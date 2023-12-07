1948413d2eae2d364b4fdb7fe8cee9a15bbe4129
mokny <33606734+mokny@users.noreply.github.com>
2023-12-07 10:59:21 +0100

    install

--- shared/install.sh
+++ shared/install.sh
@@ -2,7 +2,10 @@
 # Install with curl -sSL https://raw.githubusercontent.com/mokny/mokka/main/shared/install.sh | bash
 echo "Installing Mokka"
 cd ~
-if [ -d "~/mokka" ]; then
+
+DIR="~/mokka"
+
+if [ -d "$DIR" ]; then
     read -n1 -p "There is already a mokka directory. Remove? [Y,n]" doit 
     case $doit in  
     y|Y) rm -rf ./mokka ;; 
