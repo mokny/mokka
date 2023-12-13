c754fe7a443ae012f347596237777d09d457afdc
mokny <33606734+mokny@users.noreply.github.com>
2023-12-13 15:51:42 +0100

    pscan example

---
+++ examples/pscan/pscan.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+from datetime import datetime
+import mokkalib
+mokkalib.init()
+target = mokkalib.getOption('target')
+
+print("Scanning " + str(target))
+try:
+     
+    # will scan ports between 1 to 65,535
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+         
+        # returns an error indicator
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print("Port {} is open".format(port))
+        s.close()
+         
+except KeyboardInterrupt:
+        print("\n Exiting Program !!!!")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
